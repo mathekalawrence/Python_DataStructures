ed1eefea4161eece535f3643bd0c2c045f659b8d
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-05 16:02:50 +0300

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,14 @@
+#Sets Data Structures
+# sterilized equipment sets
+
+sterile_lab_equipment = {"Microscope", "Centrifu", "Pipettes"}
+sterile_er_equipment = {"Stethoscope", "Defibrillator", "Sutures"}
+
+#contaminated items
+contaminated = {"Defibrillator", "Sutures", "Scalpel"}
+print("Sterile ER Equipment:", sterile_er_equipment)
+print("Sterile Lab Equipment:", sterile_lab_equipment)
+
+#Concantinating/ combining
+all_sterile = sterile_lab_equipment | sterile_er_equipment
+print("All sterile Equipment:", all_sterile)
