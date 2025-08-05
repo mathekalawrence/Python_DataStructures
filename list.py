0291fe79cc686320dce8a1e86c980f54394c5e0c
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-05 15:21:21 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+
+ward_4a = ["Bed1:Lawrence", "Bed2: Moses", "Bed3: Annita"]
+
+#New admission
+ward_4a.append("Bed5: Rosy")
+ward_4a.insert(2, "Bed7: Jemoh")
+ward_4a.insert(1, "Bed3: Maryline")
+ward_4a.append("BedY: Muthioka")
+print("Ward 4a:", ward_4a)
+
+#append- at the end of the list
+#insert- to a certain index
