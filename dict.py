fae09826710a24318111908b4aafe90ddfb8848b
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-05 16:23:22 +0300

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,15 @@
+#Dictionaries
+#For patients and staff
+
+staff = {
+    "a1" : {"name": "William", "role": "nurse"}
+    "a3" : {"name": "Jane", "role": "Cleaner"}
+
+}
+
+patients = {
+    "p1": {"name": "John", "allergies": ["penicillin"]}
+    "p2": {"name": "Sally", "allergies": ["dust"]}
+}
+
+#Add new
